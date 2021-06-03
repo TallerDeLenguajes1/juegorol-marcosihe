142ea21e4aa8d706043915abe3fcb4593693660b
Marcos E. Herrera <marcosherrera.ib@gmail.com>
2021-06-03 17:46:54 -0300

    Se agruparon los datos y caracteristicas en la clase Personaje

--- Personaje.cs
+++ Personaje.cs
@@ -1,7 +1,49 @@
-namespace juegorol_marcosihe
+using System;
+using String;
+
+namespace Personajes
 {
     public class Personaje
     {
+        // Datos
+        private String tipo;
+        private String name;
+        private String apodo;
+        private int age;
+        private int health;
+        // Características
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
         
-    }
+        // Geters and Seters
+        public int Velocidad { get => velocidad; set => velocidad; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public iont nivel { get => nivel; set => nivel = value; }
+        public String Tipo { get => tipo ; set => tipo = value; }
+        public String  Name { get => name; set => name = value }
+        public String  Apodo { get => apodo; set => apodo = value}
+        public int age { get => ; set => age = value} // se debe considerar TimeSpan para calcular la edad
+        public int Health { get => health; set => health = value} //Su seteo depende de acciones o métodos
+
+        public Personaje(){
+            velocidad = 1;
+            destreza = 1;
+            fuerza = 1;
+            nivel = 1;
+            age = 15;
+            health = 100;
+        }
+
+        public Caracteristicas(int velocidad, int destreza, int destreza, int nivel){
+            this.velocidad = velocidad;
+            this.destreza = destreza;
+            this.fuerza = fuerza;
+            this.nivel = nivel;
+            this.age = age;
+            this.health = health;
+        }   
+    
 }
