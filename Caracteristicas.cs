ba0f1d771a96cd07d0181729c29fa9fc0373a676
Marcos E. Herrera <marcosherrera.ib@gmail.com>
2021-06-03 17:10:04 -0300

    Se cambio los datos por caracteristicas.

--- Caracteristicas.cs
+++ Caracteristicas.cs
@@ -5,19 +5,9 @@ namespace juegorol_marcosihe
 {
     public class Caracteristicas
     {
-        private String tipo;
-        private String name;
-        private String apodo;
-        private int age;
-        private int health;
-        DateTime bornDate = new DateTime(1850,01,13);
-        
-        public String Tipo { get => tipo ; set => tipo = value; }
-        public String  Name { get => name; set => name = value }
-        public String  Apodo { get => apodo; set => apodo = value}
-        public int age { get => ; set => age = value} // se debe considerar TimeSpan para calcular la edad
-        public int Health { get => health; set => health = value} //Su seteo depende de acciones o métodos
-
-
+        public int velocidad;
+        public int destreza;
+        public int fuerza;
+        public int nivel;
     }
 }
