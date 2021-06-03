dee98f6d965c0298557c8a668f68e4b3fa42e2e7
Marcos E. Herrera <marcosherrera.ib@gmail.com>
2021-06-03 17:13:36 -0300

    Se agregaron cambios a la clase Caracteristicas, al cual sigue en proceso.

--- Caracteristicas.cs
+++ Caracteristicas.cs
@@ -5,9 +5,12 @@ namespace juegorol_marcosihe
 {
     public class Caracteristicas
     {
-        public int velocidad;
-        public int destreza;
-        public int fuerza;
-        public int nivel;
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+
+        public int Velocidad { get => velocidad; set => velocidad; }
+        public int Destreza { get => destreza; set => destreza = value; }
     }
 }
